c6cfb0ba446ac7b1a34176e0c024048c36a8816c
Edward Tanguay (hm.win) <edward@tanguay.info>
2022-01-27 15:54:36 +0100

    list store and cart items

--- src/pages/PageItemCart.js
+++ src/pages/PageItemCart.js
@@ -1,8 +1,23 @@
 const PageItemCart = ({ items }) => {
 	return (
-		<>
-			<div className="pageItemCart">there are {items.length} items</div>
-		</>
+		<div className="pageItemCart">
+			<fieldset>
+				<legend>Store</legend>
+				{items.map((item, i) => {
+					return (
+						<div key={i} className="item">{item.name}</div>
+					)
+				})}
+			</fieldset>
+			<fieldset>
+				<legend>Item Cart</legend>
+				{items.map((item, i) => {
+					return (
+						<div key={i} className="item">{item.name}</div>
+					)
+				})}
+			</fieldset>
+		</div>
 	)
 }
 export default PageItemCart;
